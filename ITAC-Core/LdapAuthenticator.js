2339f9f353afac9ad3a9b855ce9d4aa180b0beb3
Stephane Talbot <Stephane.Talbot@univ-savoie.fr>
2017-05-30 12:58:21 +0200

    API Change: favour async functions over sync ones

--- ITAC-Core/LdapAuthenticator.js
+++ ITAC-Core/LdapAuthenticator.js
@@ -7,6 +7,27 @@ class LdapAuthenticator extends BaseAuthentification.LoginPwdAuthenticator {
 		this.ldapClient = ldap.createClient(ldapConfig.config);
 		this.ldapBaseDN = ldapConfig.baseDn;
 	}
+	/**
+	 * Methode permettant de verifier des informations d'authentifications (version synchrone).
+	 * Le methode n'est pas supporte pour l'authentification LDAP et elle renvoie tjs une exception !!!
+	 * 
+	 * @deprecated since ever
+	 * @method 
+	 * @param {Credential} credential - credential identifiant l'utilisateur a authentifier
+	 * @returns id de l'utilisteur en cas de succes, undefined sinon 
+	 * @throws {Error} la verification synchrone n'est pas supportee.
+	 */
+	verifyCredentialSync( credential){
+		throw new Error('unsupported.');
+	}
+	/**
+	 * Methode permettant de verifier des informations d'authentifications (version asynchrone).
+	 * 
+	 * @method 
+	 * @param {Credential} credential - credential identifiant l'utilisateur a authentifier
+	 * @returns {Promise} une promesse contenant l'id de l'utilisteur en cas de succesn 
+	 * @throws {Error} si la verification synchrone n'est pas supportee.
+	 */
 	verifyCredential(credential ){
 		let promise =  new Promise((resolve, reject) => {
 			if ( credential instanceof BaseAuthentification.LoginPwdCredential){
