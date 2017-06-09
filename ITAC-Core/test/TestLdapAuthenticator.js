68e688d7cd8255e7f25f3c10fb25f42264571fe7
Stephane Talbot <Stephane.Talbot@univ-savoie.fr>
2017-06-09 18:30:43 +0200

    ajout fichier de tests unitaires pour le module d'authentification LDAP

--- ITAC-Core/test/TestLdapAuthenticator.js
+++ ITAC-Core/test/TestLdapAuthenticator.js
@@ -22,8 +22,9 @@ describe("LDAP Authentication", function () {
 		}
 
 		///--- Globals
-		var SUFFIX = 'o=acme,d=fr';
-		var db = {	'uid=joe, o=acme, d=fr':{uid:'joe', userpassword:'joe', cn:'Joe Black', mail:'Joe.Black@acme.fr'},
+		var SUFFIX = 'o=acme, d=fr';
+		var db = {	'd=fr':{}, 'o=acme, d=fr':{}, 
+					'uid=joe, o=acme, d=fr':{uid:'joe', userpassword:'joe', cn:'Joe Black', mail:'Joe.Black@acme.fr'},
 					'uid=jeanne, o=acme, d=fr':{uid:'jeanne', userpassword:'12345', cn:'Jeanne Doe', mail:'Jeanne.Doe@acme.fr'} 
 		};
 		
@@ -200,74 +201,98 @@ describe("LDAP Authentication", function () {
 
 		///--- Fire it up
 		server.listen(3389, function(err) {
-		  console.log('LDAP server up at: %s', server.url);
+		  //console.log('LDAP server up at: %s', server.url);
 		  if (err) return done(err);
 		  else done();
 		});
 	});
 
 	after(function(done){
-		console.log('server ldap, open connections: '+server.connections);
+		//console.log('server ldap, open connections: '+server.connections);
 		//server.close((err)=>{if (err) done(err); else done();});  
 		// server.close don't accept callback ==> https://github.com/mcavage/node-ldapjs/issues/438
 		// so listening for 'close' event...
-		server.on('close', ()=>{console.log("ldap server closed"); done()});
+		server.on('close', ()=>{
+			//console.log("ldap server closed"); 
+			done();
+		});
 		server.close();
 	});
 	
-	describe("Constructor", function (){
-		it("Expect class Constructor to work", function(){
-			expect(new LdapAuthenticator(config)).to.exist;
-		});		
-	});
-	describe("Credential creation", function (){
-		it("Expect createCredential method to create a valide credential", function(){
-			expect((new LdapAuthenticator(config)).createCredential('Joe', "12345")).to.be.an.instanceof(BasicAuthentication.LoginPwdCredential);
-		});		
-	});
-	describe("Verifying credential", function (){
-		var authenticator;
-		before(function(){
-			authenticator = new LdapAuthenticator(config);
-		})
-		it("Expect verify credential to respond OK with valid credential - 1", function(){
-			let credential = authenticator.createCredential('uid=joe,o=acme,d=fr', 'joe');
-			return expect(authenticator.verifyCredential(credential)).to.eventually.exist;
-		});		
-		it("Expect verify credential to respond OK with valid credential - 2", function(){
-			let credential = authenticator.createCredential('uid=jeanne,o=acme,d=fr', '12345');
-			return expect(authenticator.verifyCredential(credential)).to.eventually.exist;
-		});		
-		it("Expect verify credential to fail with invalid credential - 1", function(){
-			let credential = authenticator.createCredential('uid=joe,o=acme,d=fr', '12345');
-			return expect(authenticator.verifyCredential(credential)).to.be.rejected;
-		});		
-		it("Expect verify credential to fail with invalid credential - 2", function(){
-			let credential = authenticator.createCredential('uid=jeanne,o=acme,d=fr', 'jeanne');
-			return expect(authenticator.verifyCredential(credential)).to.be.rejected;
-		});		
-		it("Expect verify credential to fail with invalid credential - 3", function(){
-			let credential = authenticator.createCredential('uid=john,o=acme,d=fr', '12345');
-			return expect(authenticator.verifyCredential(credential)).to.be.rejected;
-		});		
-		it("Expect verify credential to fail with empty password - 1", function(){
-			let credential = authenticator.createCredential('uid=joe,o=acme,d=fr', '');
-			return expect(authenticator.verifyCredential(credential)).to.be.rejected;
-		});		
-		it("Expect verify credential to fail with empty password - 2", function(){
-			let credential = authenticator.createCredential('uid=joe,o=acme,d=fr', undefined);
-			return expect(authenticator.verifyCredential(credential)).to.be.rejected;
-		});		
-		it("Expect verify credential to fail with empty password - 3", function(){
-			let credential = authenticator.createCredential('uid=joe,o=acme,d=fr');
-			return expect(authenticator.verifyCredential(credential)).to.be.rejected;
-		});		
-		it("Expect verify credential to fail with no parameters", function(){
-			return expect(authenticator.verifyCredential()).to.be.rejected;
-		});		
-		it("Expect verify credential to fail with invalid parameters", function(){
-			return expect(authenticator.verifyCredential('uid=joe,o=acme,d=fr', 'joe')).to.be.rejected;
-		});		
-	});
-	
+	describe("Generic LdapAuthenticatior", function (){
+		describe("Constructor", function (){
+			it("Expect class Constructor to work", function(){
+				expect(new LdapAuthenticator(config)).to.exist;
+			});		
+		});
+		describe("Credential creation", function (){
+			it("Expect createCredential method to create a valide credential", function(){
+				expect((new LdapAuthenticator(config)).createCredential('Joe', "12345")).to.be.an.instanceof(BasicAuthentication.LoginPwdCredential);
+			});		
+		});
+		describe("Verifying credential", function (){
+			var authenticator;
+			before(function(){
+				authenticator = new LdapAuthenticator(config);
+			})
+			it("Expect verify credential to respond OK with valid credential - 1", function(){
+				let credential = authenticator.createCredential('uid=joe,o=acme,d=fr', 'joe');
+				return expect(authenticator.verifyCredential(credential)).to.eventually.exist.and.equals('uid=joe,o=acme,d=fr');
+			});		
+			it("Expect verify credential to respond OK with valid credential - 2", function(){
+				let credential = authenticator.createCredential('uid=jeanne,o=acme,d=fr', '12345');
+				return expect(authenticator.verifyCredential(credential)).to.eventually.exist.and.equals('uid=jeanne,o=acme,d=fr');
+			});		
+			it("Expect verify credential to fail with invalid credential (invalid password) - 1", function(){
+				let credential = authenticator.createCredential('uid=joe,o=acme,d=fr', '12345');
+				return expect(authenticator.verifyCredential(credential)).to.be.rejected;
+			});		
+			it("Expect verify credential to fail with invalid credential (invalid password) - 2", function(){
+				let credential = authenticator.createCredential('uid=jeanne,o=acme,d=fr', 'jeanne');
+				return expect(authenticator.verifyCredential(credential)).to.be.rejected;
+			});		
+			it("Expect verify credential to fail with invalid credential (invalid login - dn) - 3", function(){
+				let credential = authenticator.createCredential('uid=john,o=acme,d=fr', '12345');
+				return expect(authenticator.verifyCredential(credential)).to.be.rejected;
+			});		
+			it("Expect verify credential to fail with empty password - 1", function(){
+				let credential = authenticator.createCredential('uid=joe,o=acme,d=fr', '');
+				return expect(authenticator.verifyCredential(credential)).to.be.rejected;
+			});		
+			it("Expect verify credential to fail with empty password - 2", function(){
+				let credential = authenticator.createCredential('uid=joe,o=acme,d=fr', undefined);
+				return expect(authenticator.verifyCredential(credential)).to.be.rejected;
+			});		
+			it("Expect verify credential to fail with empty password - 3", function(){
+				let credential = authenticator.createCredential('uid=joe,o=acme,d=fr');
+				return expect(authenticator.verifyCredential(credential)).to.be.rejected;
+			});		
+			it("Expect verify credential to fail with no parameters", function(){
+				return expect(authenticator.verifyCredential()).to.be.rejected;
+			});		
+			it("Expect verify credential to fail with invalid parameters", function(){
+				return expect(authenticator.verifyCredential('uid=joe,o=acme,d=fr', 'joe')).to.be.rejected;
+			});		
+		});
+		describe("Searching", function (){
+			var authenticator;
+			before(function(){
+				authenticator = new LdapAuthenticator(config);
+			})
+			it("Test search - one result", function(){
+				return expect(authenticator.search('uid=joe')).to.eventually.be.an('array').that.has.deep.members(['uid=joe, o=acme, d=fr']);
+			});		
+			it("Test search - several results", function(){
+				return expect(authenticator.search('mail=*@acme.fr'))
+					.to.eventually.be.an('array').that.has.deep.members(['uid=joe, o=acme, d=fr', 'uid=jeanne, o=acme, d=fr']);
+			});		
+			it("Test search - empty result", function(){
+				return expect(authenticator.search('uid=*Paul*'))
+					.to.eventually.be.an('array').that.is.empty;
+			});		
+			it("Test search - invalid query", function(){
+				return expect(authenticator.search('(uid=j*) & (o=acme)').catch(()=>{return [];})).to.eventually.be.empty;
+			});		
+		});
+	});	
 });
